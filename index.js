00a302c8c0a4e4ad48fbeba1c566e898696637e4
Ar3mis22 <aryashirgaonkar42@gmail.com>
2024-05-02 18:18:00 +0530

    Dashboard API, Bill API done

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const connection = require('./connection');
 const userRoute = require("./routes/user");
 const categoryRoute = require("./routes/category");
 const productRoute = require("./routes/product");
-
+const billRoute = require("./routes/bill");
+const dashboard = require("./routes/dashboard");
 const app = express();
 
 app.use(cors());
@@ -20,5 +21,7 @@ app.use(express.json());
 app.use("/user", userRoute);
 app.use("/category", categoryRoute);
 app.use("/product", productRoute);
+app.use("/bill", billRoute);
+app.use("/bill", dashboard);
 
 module.exports = app;
